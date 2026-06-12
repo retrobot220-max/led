0e3b32985f5c59c910971d3d1a6d41681768da34
YaroslavWeb <yaroslavweb98@gmail.com>
2026-06-12 16:32:44 +0800

    u

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  base: '/health-tracker/',
+  base: '/led/',
   plugins: [react(), svgr()],
 })
